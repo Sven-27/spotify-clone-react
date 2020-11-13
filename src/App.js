8df4ea75c9a85104bf05882ded8be3ccdfb4cfbe
Sven Notermans <nootje27@outlook.com>
2020-11-13 17:44:46 +0100

    Worked on token

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-
+import React, { useEffect, useState   } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
-  console.log(process.env.REACT_APP_CLIENT_ID)
+//run code based on a given condition 
+const [token, setToken] = useState(null);
+
+useEffect(() => {
+const hash = getTokenFromUrl();
+window.location.hash = "";
+
+const _token = hash.access_token;
+
+if(_token){
+  setToken(_token)
+}
+
+console.log("I have a token >>", token);
+}, []);
+
   return (
     <div className="app">
-
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
       {/*Spotify Logo*/}
 
       {/**Login with spotify button */}
-      <Login />
+      
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -4,7 +4,7 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quickstart///#
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000";
-const clientId = `${process.env.REACT_APP_CLIENT_ID}`;
+const clientId = process.env.REACT_APP_CLIENT_ID;
 
 
 const scopes = [
@@ -16,16 +16,15 @@ const scopes = [
 ];
 
 export const getTokenFromUrl = () => {
-    
     return window.location.hash
     .substring(1)
     .split("&")
     .reduce((initial, item) => {
         let parts = item.split("=");
-        initial[parts[0]] = decodeURIComponent[parts[1]];
+        initial[parts[0]] = decodeURIComponent(parts[1]);
 
         return initial;
-    }, {})
+    }, {});
 }
 console.log(process.env.REACT_APP_CLIENT_ID)
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
