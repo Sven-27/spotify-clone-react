337dd8b63dd91e01e329c96f2fa8c619a3759296
Sven Notermans <nootje27@outlook.com>
2020-11-13 11:25:34 +0100

    added env file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+
 import './App.css';
 import Login from './Login';
 
 function App() {
+  console.log(process.env.REACT_APP_CLIENT_ID)
   return (
     <div className="app">
 
--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,11 @@
+
+
+
 // https://developer.spotify.com/documentation/web-playback-sdk/quickstart///#
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000";
-const clientId = "8083f8e0bfae4ae3ba702fecc23e9ca6";
+const clientId = `${process.env.REACT_APP_CLIENT_ID}`;
+
 
 const scopes = [
     "user-read-recently-played",
@@ -12,6 +16,7 @@ const scopes = [
 ];
 
 export const getTokenFromUrl = () => {
+    
     return window.location.hash
     .substring(1)
     .split("&")
@@ -22,5 +27,5 @@ export const getTokenFromUrl = () => {
         return initial;
     }, {})
 }
-
+console.log(process.env.REACT_APP_CLIENT_ID)
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
