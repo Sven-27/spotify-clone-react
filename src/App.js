4df08f338392de814368b8b48f2c993a8fb04d3e
Sven Notermans <nootje27@outlook.com>
2020-11-16 19:55:16 +0100

    added sidebar footer and body

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { useEffect, useState   } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi  from "spotify-web-api-js";
+import Player from "./Player";
+import {useDataLayerValue} from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
 //run code based on a given condition 
-const [token, setToken] = useState(null);
+
+const [{user, token}, dispatch] = useDataLayerValue();
 
 useEffect(() => {
 const hash = getTokenFromUrl();
@@ -14,26 +20,27 @@ window.location.hash = "";
 const _token = hash.access_token;
 
 if(_token){
-  setToken(_token)
-}
 
-console.log("I have a token >>", token);
+  dispatch({
+    type: "SET_TOKEN",
+    token: _token,
+
+  })
+
+  spotify.setAccessToken(_token);
+
+  spotify.getMe().then(user => {
+    dispatch({
+      type: "SET_USER",
+      user: user,
+    })
+  })
+}
 }, []);
 
   return (
     <div className="app">
-      {
-        token ? (
-          <h1>I am logged in</h1>
-        ) : (
-          <Login />
-        )
-      }
-      {/*Spotify Logo*/}
-
-      {/**Login with spotify button */}
-      
-    </div>
+      { token ? <Player spotify={spotify} /> : <Login />}</div>
   );
 }
 
---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    //Remove token after finishing developing.
+    token: 'BQAZ1ZqyKkhpB_UYEBuUMUIUnVwGwiRPrrSt-l4ld_CkA3leoOekxqgT7qMBEbPSa4_sBPN8DX62Rm8Snlgwf9Zg3Ummxp0bFQ5E0boFFZMtL_BnxkONorgP2TMokOWQ4KG8LUP8cmJd3SpLYPznE1OHbBNt-89c',
+}
+
+const reducer = (state, action) => {
+console.log(action);
+
+switch(action.type){
+    case 'SET_USER':
+        return {
+            ...state,
+            user: action.user,
+        };
+
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token,
+            }
+        default: 
+        return state;
+}
+}
+
+export default reducer;
